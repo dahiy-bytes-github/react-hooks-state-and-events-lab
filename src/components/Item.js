fcd1a509d920a670d7dbadcdc8b419995c9c4e1d
Abdullahi <aadahiye@gmail.com>
2024-11-18 11:12:49 +0300

    Completes lab on events and state in react

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  function handleAddToCartClick() {
+    setIsInCart(isInCart => !isInCart)
+  }
+  const inCart = isInCart ? "in-cart" : ""
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart?"remove":"add"} onClick={handleAddToCartClick}>
+      {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
